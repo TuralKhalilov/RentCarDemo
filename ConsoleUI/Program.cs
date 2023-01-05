19536c4aabb4de4739f80d384450a444ddf5ddc1
Tural Khalilov <khalilov@mail.com>
2023-01-05 11:49:34 +0400

    Added Aspects

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -7,16 +7,16 @@ namespace ConsoleUI
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            CarManager carManager = new CarManager(new EfCarDal());
-            //carManager.Add(new Car { Description = "salam", ColorId = 2,  DailyPrice = 34, BrandId = 2, ModelYear = new DateTime(2023, 3, 4) });
-           var result= carManager.GetAll();
-            foreach (var car in result)
-            {
-                Console.WriteLine(car.Description);
-            }
-        }
+        //static void Main(string[] args)
+        //{
+        //    CarManager carManager = new CarManager(new EfCarDal());
+        //    //carManager.Add(new Car { Description = "salam", ColorId = 2,  DailyPrice = 34, BrandId = 2, ModelYear = new DateTime(2023, 3, 4) });
+        //   var result= carManager.GetAll();
+        //    foreach (var car in result)
+        //    {
+        //        Console.WriteLine(car.Description);
+        //    }
+        //}
     }
 }
                    
