cfceb434f759497c845d11e1f4c9fbb3c6788b4b
Tural Khalilov <khalilov@mail.com>
2023-01-08 13:51:35 +0400

    Added Helpers

---
+++ Business/Concrete/CarImageManager.cs
@@ -0,0 +1,56 @@
+﻿using Business.Abstrack;
+using Core.Utulities.Helpers.FileHelper;
+using Core.Utulities.Results;
+using DataAccess.Abstrack;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class CarImageManager : ICarImageService
+    {
+        ICarImageDal _carImageDal;
+        IFileHelper _fileHelper;
+
+        public CarImageManager(ICarImageDal carImageDal, IFileHelper fileHelper)
+        {
+            _carImageDal = carImageDal;
+            _fileHelper = fileHelper;
+        }
+
+        public IResult Add(IFormFile file, string root)
+        {
+            
+        }
+
+        public IResult Delete(CarImage carImage)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDataResult<List<CarImage>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDataResult<List<CarImage>> GetByCarId(int carId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDataResult<CarImage> GetByImageId(int imageId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IResult Update(IFormFile file, CarImage carImage)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Core/Utulities/Helpers/FileHelper/FileHelperManager.cs
@@ -0,0 +1,62 @@
+﻿using Core.Utulities.Helpers.GuidHelperr;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Utulities.Helpers.FileHelper
+{
+    public class FileHelperManager : IFileHelper
+    {
+        public void Delete(string filePath)
+        {
+            if (File.Exists(filePath))
+            {
+                File.Delete(filePath);
+            }
+           
+        }
+
+        public string Updete(IFormFile file, string filePath, string root)
+        {
+            if (File.Exists(filePath))
+            {
+                File.Delete(filePath);
+            }
+            return Upload(file, root);
+        }
+
+        public string Upload(IFormFile file, string root)
+        {
+            if (file.Length>0)
+            {
+
+
+                if (!Directory.Exists(root))
+                {
+                    Directory.CreateDirectory(root);
+                }
+
+
+                string extension = Path.GetExtension(file.FileName);
+                string guid = GuidHelper.CreateGuid();
+                string filePath = extension + guid;
+
+
+                using (FileStream fileStream = File.Create(root + filePath))
+                {
+                    file.CopyTo(fileStream);
+                    fileStream.Flush();
+                    return filePath;
+                }
+
+            }
+
+
+            return null;
+        }
+    }
+}
